a81a0b71ee7c6acea3ecc547e166d22054365cf8
nickve28 <nickvaneyk@gmail.com>
2025-04-03 23:17:50 +0200

    add simple implementation to fetch hours

--- lib/odoo_hours/client.ex
+++ lib/odoo_hours/client.ex
@@ -1,11 +1,14 @@
 defmodule OdooHours.Client do
+  require Logger
+
   @moduledoc"""
   A client to interface with the odoo API
   """
 
-  defstruct [:database, :url]
+  defstruct [:database, :url, :id, :password]
 
-  @path "/xmlrpc/2/common"
+  @auth_path "/xmlrpc/2/common"
+  @object_path "/xmlrpc/2/object"
   @user_agent %{}
 
   @doc """
@@ -20,14 +23,83 @@ defmodule OdooHours.Client do
     |> XMLRPC.encode!
 
   {:ok, %XMLRPC.MethodResponse{param: id}} =
-    HTTPoison.post!(url(config), request_body).body
+    HTTPoison.post!(auth_url(config), request_body).body
       |> XMLRPC.decode
 
   {:ok, id}
   end
 
+  def user_entries(config, id, password, options \\ []) do
+    options =
+      Keyword.merge(
+        [
+          limit: 10,
+          where: [],
+          fields: []
+        ],
+        options
+      )
+
+    execute_kw(
+      config,
+      id,
+      password,
+      "account.analytic.line",
+      "search_read",
+      [
+        [
+          ["user_id", "=", id]
+        ] ++ options[:where]
+      ],
+      %{
+        "fields" => options[:fields],
+        "limit" => options[:limit]
+      }
+    )
+    # |> atomize_keys()
+  end
+
+
+  defp execute_kw(config, id, password, model_name, method_name, params \\ [], named_params \\ %{}) do
+    %OdooHours.Client{ database: database} = config
+
+    call(config, "execute_kw", [
+      database,
+      id,
+      password,
+      model_name,
+      method_name,
+      params,
+      named_params
+    ])
+  end
+
+  defp call(config, method, params \\ []) do
+    request = %XMLRPC.MethodCall{method_name: method, params: params} |> XMLRPC.encode!()
+
+    response_body = HTTPoison.post!(url(config), request).body
+    {:ok, response} = response_body |> XMLRPC.decode()
+
+    case response do
+      %XMLRPC.MethodResponse{} ->
+        Logger.debug("response: #{inspect response.param, pretty: true}")
+        response.param
+
+      %XMLRPC.Fault{} ->
+        raise "Odoo error (code #{inspect(response.fault_code)}) #{response.fault_string}"
+
+      err ->
+        raise "#{inspect(err)}"
+    end
+  end
+
+  @spec auth_url(config: %OdooHours.Client{}) :: String.t()
+  defp auth_url(%OdooHours.Client{ url: base_url }) do
+    "#{base_url}#{@auth_path}"
+  end
+
   @spec url(config: %OdooHours.Client{}) :: String.t()
   defp url(%OdooHours.Client{ url: base_url }) do
-    "#{base_url}#{@path}"
+    "#{base_url}#{@object_path}"
   end
 end
--- lib/odoo_hours_web/controllers/page_controller.ex
+++ lib/odoo_hours_web/controllers/page_controller.ex
@@ -1,9 +1,24 @@
 defmodule OdooHoursWeb.PageController do
   use OdooHoursWeb, :controller
 
+  @url System.get_env("ODOO_URL")
+  @database System.get_env("ODOO_DB")
+
+
   def home(conn, _params) do
     external_id = conn
     |> get_session(:external_id)
+
+    password = conn
+    |> get_session(:password)
+
+    config = %OdooHours.Client{database: @database, url: @url}
+
+    OdooHours.Client.user_entries(
+      config,
+      external_id,
+      password
+    )
 
     render(conn, :home, layout: false, external_id: external_id)
   end
